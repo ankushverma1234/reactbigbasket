3580b2f453ff07001c449e4a0afb777265f74489
Ankush Kumar <vermaankush194@gmail.com>
2025-05-16 22:32:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,57 +10,53 @@ import './nonveg.css';
 import './milk.css';
 import './chocolate.css';
 
-
- 
 import Home from './Home';
-import Chocolate from './chocolate';
 import Signing from './signing';
 import Order from './order';
 import AboutUs from './aboutUs';
 import ContactUs from './contactUs';
 import Milk from './milk';
- 
+
 import NonVeg from './nonVeg';
 import Cart from './cart';
 import Veg from './Veg';
-
+import Chocolate from './chocolate';
+ 
 
 function App() {
-
-  const cart = useSelector((state) => state.cart); // 👈 GET cart from Redux
-  const cartCount = cart.reduce((total, item) => total + item.quantity, 0); // 👈 Count total items
-
+  const cart = useSelector((state) => state.cart); // get cart from Redux
+  const cartCount = cart.reduce((total, item) => total + item.quantity, 0); // total items count
 
   return (
     <BrowserRouter>
       <header className="app-header">
         <h1>🛒 BigBasket</h1>
         <nav className="nav-links">
-          <Link to="/Home">Home</Link>
-          <Link to="/Nonveg">NonVeg</Link>
-          <Link to="/Veg">Veg</Link>
-          <Link to="/Milk">Milk</Link>
-          <Link to="/Chocolate">Chocolate</Link>
-          <Link to="/Signing">Sign In</Link>
-          <Link to="/Cart">Cart ({cartCount})</Link>
+          <Link to="/home">Home</Link>
+          <Link to="/nonveg">NonVeg</Link>
+          <Link to="/veg">Veg</Link>
+          <Link to="/milk">Milk</Link>
+          <Link to="/chocolate">Chocolate</Link>
+          <Link to="/signing">Sign In</Link>
+          <Link to="/cart">Cart ({cartCount})</Link>
           <Link to="/orders">Orders</Link>
-          <Link to="/AboutUs">About Us</Link>
-          <Link to="/ContactUs">Contact Us</Link>
+          <Link to="/aboutus">About Us</Link>
+          <Link to="/contactus">Contact Us</Link>
         </nav>
       </header>
 
       <main className="main-content">
         <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/NonVeg" element={<NonVeg />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/nonveg" element={<NonVeg />} />
           <Route path="/veg" element={<Veg />} />
-          <Route path="/Milk" element={<Milk />} />
-          <Route path="/Chocolate" element={<Chocolate />} />
-          <Route path="/Signing" element={<Signing />} />
-          <Route path="/Cart" element={<Cart />} />
-          <Route path="/Orders" element={<Order />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/ContactUs" element={<ContactUs />} />
+          <Route path="/milk" element={<Milk />} />
+          <Route path="/chocolate" element={<Chocolate />} />
+          <Route path="/signing" element={<Signing />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/orders" element={<Order />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contactus" element={<ContactUs />} />
           <Route path="/" element={<Home />} />
         </Routes>
       </main>
